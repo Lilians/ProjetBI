ebafb1ef8264cd49937d582117ba27ce54486470
Vlad <vladmourier@hotmail.fr>
2016-09-09 21:27:21 +0200

    Proposition pour la BDD - Ne prend pas en compte la position (pour le moment)

---
+++ Db/DB.sql
@@ -0,0 +1,34 @@
+Create database projetbi;
+
+CREATE TABLE contrat (
+    contrat_name VARCHAR(100) NOT NULL PRIMARY KEY,
+    commercial_name VARCHAR(100) NOT NULL,
+    country_code VARCHAR(10)
+);
+CREATE TABLE cities (
+    city_id INT NOT NULL PRIMARY KEY,
+    city_name VARCHAR(50),
+    contrat_name VARCHAR(100) NOT NULL,
+    FOREIGN KEY (contrat_name)
+        REFERENCES contrat (contrat_name)
+);
+CREATE TABLE station (
+    station_number INT NOT NULL PRIMARY KEY,
+    contrat_name VARCHAR(100) NOT NULL,
+    station_name VARCHAR(100),
+    address VARCHAR(200),
+    banking BOOLEAN,
+    bonus BOOLEAN,
+    bike_stands INT,
+    FOREIGN KEY (contrat_name)
+        REFERENCES contrat (contrat_name)
+);
+CREATE TABLE station_snapshot (
+    station_snapshot_id INT NOT NULL PRIMARY KEY,
+    station_number INT NOT NULL,
+    available_bike_stands INT NOT NULL,
+    available_bikes INT NOT NULL,
+    last_update DATETIME NOT NULL,
+    FOREIGN KEY (station_number)
+        REFERENCES station (station_number)
+)

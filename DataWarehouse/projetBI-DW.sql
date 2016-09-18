e5010839ce0d0d429ad3f1769f6ff51ceb34b03d
Bertrand Choubert <bertrand.choubert@hotmail.fr>
2016-09-18 20:14:39 +0200

    Push #14 : Created Data Waresouse SQL Script

---
+++ DataWarehouse/projetBI-DW.sql
@@ -0,0 +1,68 @@
+
+use `projetbi_dw`;
+
+CREATE TABLE DistrictCity (
+	district_city_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	district VARCHAR(100),
+	city VARCHAR(100),
+	country VARCHAR(100)
+);
+
+CREATE TABLE StationStreet (
+	station_street_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	district_city_id INT,
+	station_name VARCHAR(100),
+	address VARCHAR(150),
+	banking boolean,
+	bonus boolean,
+	nb_emplacements INT,
+	street VARCHAR(150),
+	FOREIGN KEY (district_city_id)
+		REFERENCES DistrictCity(district_city_id)
+);
+
+CREATE TABLE Contract (
+	contract_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	commercial_name VARCHAR(50)
+);
+
+CREATE TABLE Weather (
+	weather_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	temperature INT,
+	weather_label VARCHAR(50)
+);
+
+CREATE TABLE DayWeek (
+	day_week_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`day` INT,
+	week INT,
+	`month` INT,
+	`year` INT
+);
+
+CREATE TABLE MinuteHour (
+	minute_hour_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	day_week_id INT,
+	`hour` INT,
+	`minute` INT,
+	FOREIGN KEY (day_week_id)
+		REFERENCES DayWeek(day_week_id)
+);
+
+CREATE TABLE Snapshot (
+	snapshot_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	station_street_id INT,
+	contract_id INT,
+	minute_hour_id INT,
+	weather_id INT,
+	available_bike_stands INT,
+	available_bikes INT,
+	FOREIGN KEY (station_street_id)
+		REFERENCES StationStreet(station_street_id),
+	FOREIGN KEY (contract_id)
+		REFERENCES Contract(contract_id),
+	FOREIGN KEY (minute_hour_id)
+		REFERENCES MinuteHour(minute_hour_id),
+	FOREIGN KEY (weather_id)
+		REFERENCES Weather(weather_id)
+);
